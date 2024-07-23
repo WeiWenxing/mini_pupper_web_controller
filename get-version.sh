32ed4ba7bea26bf45aaf48c195a90f6a1c78ffa1
weiwx <weiwx@mangdang.net>
2024-07-23 12:24:00 +0800

    [Fix] fix install on pupper1 QuadrupedRobot repo

---
+++ get-version.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+TAG_COMMIT=$(git rev-list --abbrev-commit --tags --max-count=1)
+TAG=$(git describe --abbrev=0 --tags ${TAG_COMMIT} 2>/dev/null || true)
+COMMIT=$(git rev-parse --short HEAD)
+DATE=$(git log -1 --format=%cd --date=format:"%Y%m%d")
+VERSION=$(echo ${TAG} | sed "s/^v//")
+if [[ ! "${COMMIT}" = "${TAG_COMMIT}" ]];
+then
+  VERSION=$(echo ${VERSION}-next-${COMMIT}-${DATE})
+fi
+if [[ "${VERSION}" = "" ]];
+then
+ VERSION=${COMMIT}-${DATA}
+fi
+if [[ $(git status --porcelain|wc -l) > 0 ]];
+then
+  VERSION=${VERSION}-dirty
+fi
+echo ${VERSION}
+MAJOR_VERSION=$(echo $VERSION | cut -d'-' -f1)
+echo ${MAJOR_VERSION}
